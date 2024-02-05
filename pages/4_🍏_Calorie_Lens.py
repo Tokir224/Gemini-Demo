2ebfa15c22b239a73e44909e89b199a8f92e17b6
Tokir-Vora <tokirvora.224@gmail.com>
2024-02-05 13:54:17 +0530

    calorie lens prompt improvment

--- pages/4_🍏_Calorie_Lens.py
+++ pages/4_🍏_Calorie_Lens.py
@@ -1,22 +1,21 @@
 ### Health Management APP
+from dotenv import load_dotenv
+
+load_dotenv() ## load all the environment variables
+
 import streamlit as st
 import os
 import google.generativeai as genai
-
 from PIL import Image
-from dotenv import load_dotenv
-
-load_dotenv()  ## load all the environment variables
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-
 ## Function to load Google Gemini Pro Vision API And get response
-def get_gemini_repsonse(input, image, prompt):
-    model = genai.GenerativeModel("gemini-pro-vision")
-    response = model.generate_content([input, image[0], prompt])
-    return response.text
 
+def get_gemini_repsonse(input,image):
+    model=genai.GenerativeModel('gemini-pro-vision')
+    response=model.generate_content([input,image[0]])
+    return response.text
 
 def input_image_setup(uploaded_file):
     # Check if a file has been uploaded
@@ -27,29 +26,28 @@ def input_image_setup(uploaded_file):
         image_parts = [
             {
                 "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
-                "data": bytes_data,
+                "data": bytes_data
             }
         ]
         return image_parts
     else:
         raise FileNotFoundError("No file uploaded")
-
-
+    
 ##initialize our streamlit app
-st.set_page_config(page_title="Calorie Lens")
-st.header("Calories Insight through Imagery")
 
-input = st.text_input("Input Prompt: ", key="input")
+st.set_page_config(page_title="Gemini Health App")
+
+st.header("Gemini Health App")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-image = ""
+image=""   
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image.", use_column_width=True)
 
 
-submit = st.button("Tell me the total calories")
+submit=st.button("Tell me the total calories")
 
-input_prompt = """
+input_prompt="""
 You are an expert in nutritionist where you need to see the food items from the image
                and calculate the total calories, also provide the details of every food items with calories intake
                is below format
@@ -58,13 +56,14 @@ You are an expert in nutritionist where you need to see the food items from the 
                2. Item 2 - no of calories
                ----
                ----
-
-
+        Finally you can also mention whether the food is helthy or not also mention the percentage split of
+        the ratio of carbohydrates, fats, fibers, sugar and other important things required in our diet
 """
 
 ## If submit button is clicked
+
 if submit:
-    image_data = input_image_setup(uploaded_file)
-    response = get_gemini_repsonse(input_prompt, image_data, input)
+    image_data=input_image_setup(uploaded_file)
+    response=get_gemini_repsonse(input_prompt,image_data)
     st.subheader("The Response is")
     st.write(response)
